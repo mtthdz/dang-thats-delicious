8d92f21397a278a22121360bce9534fe5de90f0a
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-09-28 17:16:57 -0400

    added flashes

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,6 +1,12 @@
+const mongoose = require('mongoose'); 
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-  // console.log(req.name);
+  console.log(req.name);
+  req.flash('error', 'something happened');
+  req.flash('info', 'something happened');
+  req.flash('warning', 'something happened');
+  req.flash('success', 'something happened');
   res.render('index');
 };
 
@@ -8,6 +14,10 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store'});
 }
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  req.flash('success', `successfully created ${store.name}. care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+
 }
