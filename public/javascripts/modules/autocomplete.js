a3ce3a067da0f4e5f40994e09284d46439fb03ca
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-10-28 18:43:00 -0400

    fixed google maps api issues

--- public/javascripts/modules/autocomplete.js
+++ public/javascripts/modules/autocomplete.js
@@ -1,6 +1,8 @@
 function autocomplete(input, latinput, lnginput) {
   // Google uses lat-lng, while mongoDB uses lng-lat
   // skip fn from running if no input on page
+  // Remember to enable both Maps & places API
+  // consider adding an IP address restriction once live
   if(!input) return;
 
   const dropdown = new google.maps.places.Autocomplete(input);
