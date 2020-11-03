5525b15dcda14db653cc13cd874fab908fcf1a6d
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-03 16:56:46 -0500

    wrapped up tags page

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -70,7 +70,14 @@ exports.getStoreBySlug = async(req, res, next) => {
 exports.getStoresByTag = async(req, res) => {
   // custom static method
   // created in Store.js 
-  const tags = await Store.getTagsList();
   const tag = req.params.tag;
-  res.render('tag', { tags, title: 'Tags', tag });
+  const tagQuery = tag || { $exists: true };
+  const tagsPromise = Store.getTagsList();
+  const storesPromise = Store.find({ tags: tagQuery });
+
+  // created two promises and awaited both via promise.all
+  // stores response in its own variables
+  const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+  
+  res.render('tag', { tags, title: 'Tags', tag, stores });
 }
