55f684e61d907d61e5a55bb5f18d94aa875ef1ba
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-11-09 15:40:25 -0500

    added password reset email functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const promisify = require('es6-promisify');
+const mail = require('../handlers/mail');
 
 // passport doesn't need req res arguments
 exports.login = passport.authenticate('local', {
@@ -44,7 +45,13 @@ exports.forgot = async (req, res) => {
 
   // 3. send email with reset token
   const resetURL = `http://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
-  req.flash('success', `test response ${resetURL}`);
+  await mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset',
+  });
+  req.flash('success', `You have been emailed a password reset link.`);
   
   // 4. redirect to login page
   res.redirect('/login');
